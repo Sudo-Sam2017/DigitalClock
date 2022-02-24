08a7cadf20025a57438714aaf2c337eda29b0501
Sudo-Sam2017 <31296306+Sudo-Sam2017@users.noreply.github.com>
2022-02-23 23:21:11 -0500

    Added running seconds to current time. Added date info

--- digitalClock.js
+++ digitalClock.js
@@ -1,2 +1,31 @@
-const d = new Date();
-document.getElementById("currentTime").value = d;
+/* const d = new Date();
+document.getElementById("currentTime").value = d; */
+
+var span = document.getElementById('span');
+
+function time() {
+  var d = new Date();
+  var hours = d.getHours() > 12 ? d.getHours() - 12 : d.getHours();
+        var am_pm = d.getHours() >= 12 ? "PM" : "AM";
+        hours = hours < 10 ? "0" + hours : hours;
+        var minutes = d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes();
+        var seconds = d.getSeconds() < 10 ? "0" + d.getSeconds() : d.getSeconds();
+  span.textContent = 
+    ("0" + hours).substr(-2) + ":" + ("0" + minutes).substr(-2) + ":" + ("0" + seconds).substr(-2) + ' ' + am_pm;
+ d.toLocaleString('en-us', {hour: 'numeric', minute:'numeric', hour12: true})
+
+}
+
+function getDate() {
+    var today = new Date();
+    var day = String(today.getDate()).padStart(2, '0');
+    var month = String(today.getMonth() + 1).padStart(2, '0');
+    var year = today.getFullYear();
+    today = month + '-' + day + '-' + year;
+    var dateBox = document.getElementById('date');
+    dateBox.textContent = today;
+}
+
+
+setInterval(time, 1000);
+getDate();
